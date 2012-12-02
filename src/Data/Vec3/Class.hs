9456c244cff50c20b45963707e43b8dc7c8c1486
Dmitry Dzhus <dima@dzhus.org>
2012-12-03 03:47:12 +0400

    Reimport matrix functions to class

--- src/Data/Vec3/Class.hs
+++ src/Data/Vec3/Class.hs
@@ -6,29 +6,97 @@ module Data.Vec3.Class
 
 where
 
+-- | Three-dimensional vector and matrix.
 class Vec3 v where
-    origin    :: v
-    fromXYZ   :: Double -> Double -> Double -> v
+    data Matrix v
 
-    (<+>)     :: v -> v -> v
-    (<->)     :: v -> v -> v
-    (><)      :: v -> v -> v
-    (.^)      :: v -> Double -> v
-    (.*)      :: v -> v -> Double
+    -- | Origin point @(0, 0, 0)@.
+    origin         :: v
 
-    norm      :: v -> Double
-    norm v = sqrt (v .* v)
+    -- | Construct a new vector from components.
+    fromXYZ        :: (Double, Double, Double) -> v
+
+    -- | Deconstruct a vector into components.
+    toXYZ          :: v -> (Double, Double, Double)
+
+    -- | Add two vectors.
+    (<+>)          :: v -> v -> v
+
+    -- | Subtract two vectors.
+    (<->)          :: v -> v -> v
+
+    -- | Cross product.
+    (><)           :: v -> v -> v
+
+    -- | Scale a vector.
+    (.^)           :: v -> Double -> v
+
+    -- | Dot product.
+    (.*)           :: v -> v -> Double
+
+    -- | Euclidean norm of a vector.
+    norm           :: v -> Double
+    norm v          = sqrt (v .* v)
     {-# INLINE norm #-}
 
-    normalize :: v -> v
-    normalize v = v .^ (1 / norm v)
+    -- | Produce unit vector with the same direction as the original
+    -- one.
+    normalize      :: v -> v
+    normalize v     = v .^ (1 / norm v)
     {-# INLINE normalize #-}
 
-    distance  :: v -> v -> Double
-    distance v1 v2 = norm (v1 <-> v2)
+    -- | Distance between two points.
+    distance       :: v -> v -> Double
+    distance v1 v2  = norm (v1 <-> v2)
     {-# INLINE distance #-}
 
-    invert    :: v -> v
-    invert v  =  v .^ (-1)
+    -- | Scale vector by -1.
+    invert         :: v -> v
+    invert v        = v .^ (-1)
     {-# INLINE invert #-}
 
+
+    -- | Construct a new matrix from rows.
+    fromRows       :: (v, v, v) -> Matrix v
+
+    -- | Deconstruct a matrix into rows.
+    toRows         :: Matrix v -> (v, v, v)
+
+    -- | Generic vector dot product.
+    --
+    -- Multiply the transpose of the first vector by the given matrix,
+    -- then multiply the result by the second vector.
+    dotM           :: v -> v -> Matrix v -> Double
+    dotM v1 v2 m    = v1 .* (m `mxv` v2)
+    {-# INLINE dotM #-}
+
+    -- | Multiply matrix and vector.
+    mxv            :: Matrix v -> v -> v
+    mxv m v         = case toRows m of
+                        (r1, r2, r3) -> fromXYZ (r1 .* v, r2 .* v, r3 .* v)
+    {-# INLINE mxv #-}
+
+    -- | Build a diagonal matrix from a number.
+    diag           :: Double -> Matrix v
+    diag d          = fromRows
+                      (fromXYZ (d, 0, 0),
+                       fromXYZ (0, d, 0),
+                       fromXYZ (0, 0, d))
+    {-# INLINE diag #-}
+
+    -- | Transpose vector and multiply it by another vector, producing a
+    -- matrix.
+    vxv            :: v -> v -> Matrix v
+    vxv v1 v2       = case toXYZ v1 of
+                        (v11, v12, v13) ->
+                            fromRows (v2 .^ v11, v2 .^ v12, v2 .^ v13)
+    {-# INLINE vxv #-}
+
+    -- | Add two matrices.
+    addM           :: Matrix v -> Matrix v -> Matrix v
+    addM m1 m2      = case (toRows m1, toRows m2) of
+                        ((r11, r12, r13),
+                         (r21, r22, r23)) -> fromRows (r11 <+> r21,
+                                                       r12 <+> r22,
+                                                       r13 <+> r23)
+    {-# INLINE addM #-}
--- src/Data/Vec3/Unboxed.hs
+++ src/Data/Vec3/Unboxed.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {-|
 
@@ -8,13 +9,6 @@ Suitable for use with "Data.Vector.Unboxed".
 
 module Data.Vec3.Unboxed
     ( UVec3(..)
-    -- * Matrix operations
-    , Matrix(..)
-    , mxv
-    , vxv
-    , dotM
-    , diag
-    , addM
     )
 
 where
@@ -36,11 +30,22 @@ newtype UVec3 = UVec3 (Double, Double, Double)
 
 
 instance Vec3 UVec3 where
+    newtype Matrix UVec3 = UMatrix (UVec3, UVec3, UVec3)
+
     origin = UVec3 (0, 0, 0)
     {-# INLINE origin #-}
 
-    fromXYZ x y z = UVec3 (x, y, z)
+    fromXYZ v = UVec3 v
     {-# INLINE fromXYZ #-}
+
+    toXYZ (UVec3 v) = v
+    {-# INLINE toXYZ #-}
+
+    fromRows (r1, r2, r3) = UMatrix (r1, r2, r3)
+    {-# INLINE fromRows #-}
+
+    toRows (UMatrix (r1, r2, r3)) = (r1, r2, r3)
+    {-# INLINE toRows #-}
 
     (<+>) (UVec3 (x1, y1, z1)) (UVec3 (x2, y2, z2)) = 
         UVec3 (x1 + x2, y1 + y2, z1 + z2)
@@ -61,44 +66,3 @@ instance Vec3 UVec3 where
     (.*) (UVec3 (x1, y1, z1)) (UVec3 (x2, y2, z2)) = 
         x1 * x2 + y1 * y2 + z1 * z2
     {-# INLINE (.*) #-}
-
-
--- | Matrix given by its rows.
-newtype Matrix = Matrix (UVec3, UVec3, UVec3)
-    deriving (Eq, Show,
-              VG.Vector VU.Vector,
-              VG.MVector VU.MVector,
-              VU.Unbox)
-
-
--- | Generic vector dot product.
---
--- Multiply transpose of first vector by given matrix, then multiply
--- the result by second vector.
-dotM :: UVec3 -> UVec3 -> Matrix -> Double
-dotM v1 v2 m = v1 .* (m `mxv` v2)
-
-
--- | Multiply matrix (given by row vectors) and vector
-mxv :: Matrix -> UVec3 -> UVec3
-mxv (Matrix (r1, r2, r3)) v = UVec3 (r1 .* v, r2 .* v, r3 .* v)
-
-
--- | Produce matrix with diagonal elements equal to given value.
-diag :: Double -> Matrix
-diag d = Matrix (UVec3 (d, 0, 0),
-                 UVec3 (0, d, 0),
-                 UVec3 (0, 0, d))
-
-
--- | Transpose vector and multiply it by another vector, producing a
--- matrix.
-vxv :: UVec3 -> UVec3 -> Matrix
-vxv (UVec3 (v11, v12, v13)) v2 = Matrix (v2 .^ v11, v2 .^ v12, v2 .^ v13)
-
-
--- | Add two matrices.
-addM :: Matrix -> Matrix -> Matrix
-addM (Matrix (r11, r12, r13)) (Matrix (r21, r22, r23)) =
-    -- We could add Applicative instance for Matrix and lift (+) to it.
-    Matrix (r11 <+> r21, r12 <+> r22, r13 <+> r23)
