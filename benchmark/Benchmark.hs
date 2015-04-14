9db043d977329a1b954684e7a85e44da87e5f9e9
Dmitry Dzhus <dima@dzhus.org>
2015-04-15 00:20:33 +0300

    Drop Unbox SVec3, fixing #1
    It's too complex for vector-th-unbox as it requires index
    transformations. Its performance never differed much from Storable-based
    vector of SVec3 anyways.
    
    Fix benchmark to use criterion-1.1

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -28,44 +28,31 @@ foo v1 v2 = x1 + x2
     where
       (x1, _, _) = toXYZ v1
       (x2, _, _) = toXYZ v2
-{-# INLINE foo #-}
 
 
 -- Access to whole elements
-test :: (Show a, Vec3 a, VG.Vector v a) => v a -> v a -> IO ()
-test v v2 = do
-  let !vr = VG.zipWith (<+>) v v
-  return ()
-{-# INLINE test #-}
+testWhole :: (Show a, Vec3 a, VG.Vector v a) => v a -> v a -> Benchmarkable
+testWhole v v2 = (uncurry $ VG.zipWith (<+>)) `whnf` (v, v2)
 
 
 -- Access to x components of elements
-testComp :: (Show a, Vec3 a, VG.Vector v a, VG.Vector v Double) => v a -> v a -> IO ()
-testComp v v2 = do
-  let !vr = VG.zipWith (foo) v v
-  return ()
-{-# INLINE testComp #-}
+testComp :: (Show a, Vec3 a, VG.Vector v a, VG.Vector v Double) => v a -> v a -> Benchmarkable
+testComp v v2 = (uncurry $ VG.zipWith foo) `whnf` (v, v2)
 
 
 -- zipWith using generic dot product
-testDotM :: (Show a, Vec3 a, VG.Vector v a, VG.Vector v Double) => v a -> v a -> IO ()
-testDotM v v2 = do
-  let o = fromXYZ oXYZ
-      m = fromRows (o, o, o)
-      dotM' e1 e2 = dotM e1 e2 m
-      !vr = VG.zipWith dotM' v v2
-  return ()
-{-# INLINE testDotM #-}
+testDotM :: (Show a, Vec3 a, VG.Vector v a, VG.Vector v Double) => v a -> v a -> Benchmarkable
+testDotM v v2 = (uncurry $ VG.zipWith dotM') `whnf` (v, v2)
+  where
+    o = fromXYZ oXYZ
+    m = fromRows (o, o, o)
+    dotM' e1 e2 = dotM e1 e2 m
 
 
 -- Note that source arrays are not forced in test functions.
 
 tv :: VU.Vector UVec3
 tv = VG.replicate n $ fromXYZ oXYZ
-
-
-uv :: VU.Vector SVec3
-uv = VG.replicate n $ fromXYZ oXYZ
 
 
 sv :: VS.Vector SVec3
@@ -76,20 +63,12 @@ tv' :: VU.Vector UVec3
 tv' = VG.replicate n $ fromXYZ oXYZ'
 
 
-uv' :: VU.Vector SVec3
-uv' = VG.replicate n $ fromXYZ oXYZ'
-
-
 sv' :: VS.Vector SVec3
 sv' = VG.replicate n $ fromXYZ oXYZ'
 
 
 bsv :: VS.Vector SVec3
 bsv = VG.replicate bigN $ fromXYZ oXYZ
-
-
-buv :: VU.Vector SVec3
-buv = VG.replicate bigN $ fromXYZ oXYZ
 
 
 btv :: VU.Vector UVec3
@@ -100,10 +79,6 @@ bsv' :: VS.Vector SVec3
 bsv' = VG.replicate bigN $ fromXYZ oXYZ'
 
 
-buv' :: VU.Vector SVec3
-buv' = VG.replicate bigN $ fromXYZ oXYZ'
-
-
 btv' :: VU.Vector UVec3
 btv' = VG.replicate bigN $ fromXYZ oXYZ'
 
@@ -111,33 +86,27 @@ btv' = VG.replicate bigN $ fromXYZ oXYZ'
 main :: IO ()
 main = defaultMain
        [ bgroup "zipWith"
-                    [ bench "SVec/Storable"        $ test sv sv'
-                    , bench "SVec/Unboxed"         $ test uv uv'
-                    , bench "UVec/Unboxed"         $ test tv tv'
+                    [ bench "SVec/Storable"        $ testWhole sv sv'
+                    , bench "UVec/Unboxed"         $ testWhole tv tv'
                     ]
        , bgroup "zipWith-by-x"
                     [ bench "SVec/Storable"        $ testComp sv sv'
-                    , bench "SVec/Unboxed"         $ testComp uv uv'
                     , bench "UVec/Unboxed"         $ testComp tv tv'
                     ]
        , bgroup "zipWith-dotM"
                     [ bench "SVec/Storable"        $ testDotM sv sv'
-                    , bench "SVec/Unboxed"         $ testDotM uv uv'
                     , bench "UVec/Unboxed"         $ testDotM tv tv'
                     ]
        , bgroup "zipWith 10M"
-                    [ bench "SVec/Storable"        $ test bsv bsv'
-                    , bench "SVec/Unboxed"         $ test buv buv'
-                    , bench "UVec/Unboxed"         $ test btv btv'
+                    [ bench "SVec/Storable"        $ testWhole bsv bsv'
+                    , bench "UVec/Unboxed"         $ testWhole btv btv'
                     ]
        , bgroup "zipWith-by-x 10M"
                     [ bench "SVec/Storable"        $ testComp bsv bsv'
-                    , bench "SVec/Unboxed"         $ testComp buv buv'
                     , bench "UVec/Unboxed"         $ testComp btv btv'
                     ]
        , bgroup "zipWith-dotM 10M"
                     [ bench "SVec/Storable"        $ testDotM bsv bsv'
-                    , bench "SVec/Unboxed"         $ testDotM buv buv'
                     , bench "UVec/Unboxed"         $ testDotM btv btv'
                     ]
        ]
