70f11cd14d7ce60d6ad1bf5220ef3deac487987c
Dmitry Dzhus <dima@dzhus.org>
2019-06-10 09:33:33 +0100

    refactor: Remove redundant TypeSynonymInstances pragma

--- src/Data/Vec3/Class.hs
+++ src/Data/Vec3/Class.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 
 module Data.Vec3.Class
     ( Vec3(..)
